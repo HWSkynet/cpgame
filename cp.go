2a274b4a37b09d1d9cb33322763e89d126a6924f
Nigh <jiyucheng007@gmail.com>
2017-12-26 13:54:36 +0800

    添加DND骰子
    Signed-off-by: Nigh <jiyucheng007@gmail.com>

--- cp.go
+++ cp.go
@@ -14,13 +14,40 @@ import "regexp"
 const (
 	GN_Success int = iota
 )
+
+type Dice string
+
+var diceexp *regexp.Regexp
+
 func NewGame() {
+	diceexp, _ = regexp.Compile("(\\d+)(?:D|d)(\\d+)")
 	rand.Seed(time.Now().UnixNano())
 }
 
 func RandInt(n int) int {
 	return rand.Intn(n)
 }
+
+func (self Dice) Value() (sum int) {
+	strs := diceexp.FindStringSubmatch(string(self))
+	// strs[0]似乎是占位
+	if len(strs) < 3 {
+		panic("xDy格式错误")
+	}
+	x, _ := strconv.Atoi(strs[1])
+	y, _ := strconv.Atoi(strs[2])
+	//fmt.Printf("%d|%dD%d\n", len(strs), x, y)
+	sum = 0
+	for i := 0; i < x; i++ {
+		sum += rand.Intn(y) + 1
+	}
+	return
+}
+func (self Dice) Add(a int) (sum int) {
+	sum = self.Value() + a
+	return
+}
+
 func Version() string {
 	return "0.0.0"
 }
